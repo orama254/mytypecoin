a75c00e462fa3d48b9a48c8c28e1ec399b5a68e7
orama254 <orama209@gmail.com>
2021-02-03 13:24:19 +0300

    Workout:react with typescript project init

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,57 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react';
+import { useQuery } from 'react-query';
+
+//Styles
+import { Wrapper } from './styles/App.styles' 
+
+type CoinData = {
+  '15m': number;
+  buy: number;
+  last:number;
+  sell: number;
+  symbol: string;
+}
+
+type Currencies = {
+  [key: string]: CoinData;
+}
+
+const getCoinData = async (): Promise<Currencies> =>
+  await(await fetch('https://blockchain.info/ticker')).json();
+
+  const INTERVAL_TIME = 30000 //time = 30seconds
 
 function App() {
+  const [ currency, setCurrency ] = useState('USD');
+
+  const { data, isLoading, error } = useQuery<Currencies>('bc-data', getCoinData,{refetchInterval:INTERVAL_TIME});
+
+  const handleCurrencySelection = (e: any) =>{
+    setCurrency(e.currentTarget.value)
+  }
+
+  if(isLoading) return <div>The File is loading...</div>
+  if(error) return <div>Something went terribly wrong...sorry</div>
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Wrapper>
+      <>
+        <h2>Bitcoin Price</h2>
+        <select value={currency} onChange={handleCurrencySelection}>
+          {data && Object.keys(data).map(currency => (
+            <option key={currency} value={currency}> 
+                {currency}
+            </option>
+            ))}
+        </select>
+        <div>
+          <h2>
+            {data && data[currency].symbol}
+            {data && data[currency].last}
+          </h2>
+        </div>
+      </>
+    </Wrapper>
   );
 }
 
